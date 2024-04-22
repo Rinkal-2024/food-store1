da0aebd414dbcc847e9eb18cee5675eb68d44584
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2024-04-22 14:13:11 +0530

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,20 +4,25 @@ dotenv.config();
 import express from "express";
 import cors from "cors"
 import jwt from "jsonwebtoken"
-import { sample_foods, sample_tags, sample_users } from "./src/data";
-import foodRouter from './src/router/food.router';
-import userRouter from './src/router/user.router';
-import { dbConnect } from './src/configs/database.config';
-import orderRouter from './src/router/order.router';
+import { sample_foods, sample_tags, sample_users } from "./data";
+import foodRouter from './router/food.router';
+import userRouter from './router/user.router';
+import { dbConnect } from './configs/database.config';
+import orderRouter from './router/order.router';
 dbConnect();
 
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-    credentials : true,
-    origin : ["https://food-store1-master.vercel.app"]
-}));
+// app.use(cors({
+//     credentials : true,
+//     origin : ["https://food-store1-master.vercel.app"]
+// }));
+app.use(cors())
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/api/food" ,foodRouter);
 app.use("/api/users" ,userRouter);
