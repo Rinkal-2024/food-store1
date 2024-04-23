6cb5c6a6c0ed57d3d7bcc1eecf03c73fe18c1162
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2024-04-23 15:02:09 +0530

    update files

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -3,8 +3,8 @@ import {connect ,ConnectOptions } from "mongoose";
 
 export const dbConnect = () =>{
     connect('mongodb+srv://rinkal:FkDHnl00inldHlaS@cluster0.k3xiiif.mongodb.net/Food-Store?retryWrites=true&w=majority&appName=Cluster0',{
-        // userNewUrlParser : true,
-        // userUniFiedTopology :true
+        //  userNewUrlParser : true,
+        //  userUniFiedTopology :true
 
     } as ConnectOptions).then(
         () => console.log("connect successfully"),
