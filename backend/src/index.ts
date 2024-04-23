aaaec31f8ec2457e314c0a7437f8a216b5a2798c
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2024-04-23 15:50:27 +0530

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,9 @@
 import dotenv from 'dotenv'
 dotenv.config();
 require('dotenv').config();
-import express from "express";
+// import express from "express";
+const express = require('express')
 import cors from "cors"
-import jwt from "jsonwebtoken"
-import { sample_foods, sample_tags, sample_users } from "./data";
 import foodRouter from './router/food.router';
 import userRouter from './router/user.router';
 import { dbConnect } from './configs/database.config';
@@ -15,16 +14,6 @@ const app = express();
 app.use(express.json());
 
 
-
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', 'https://food-store1-alpha.vercel.app');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     res.setHeader('Access-Control-Allow-Credentials', '');
-//     next();
-//   });
-
-// app.use(cors())
 app.use(cors({
     credentials : true,
     origin : ["https://food-store1-master.vercel.app"]
@@ -34,11 +23,11 @@ app.use("/api/food" ,foodRouter);
 app.use("/api/users" ,userRouter);
 app.use("/api/orders" ,orderRouter);
 
-app.get('/' , (req,res)=>{
+app.get('/' , (_req: any,res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): void; new(): any; }; }; })=>{
     res.status(200).send('hello')
 })
 
-app.get('/home', (req,res)=>{
+app.get('/home', (_req: any,res: { send: (arg0: string) => void; })=>{
     res.send('hii')
 })
 
